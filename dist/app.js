c2b5f5a1808026b29eecf0e70820cdc50c36a21b
Md. Ariful Alam Nishan <arif.nishan2011@gmail.com>
2023-08-12 13:20:21 +0600

    setting todo

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,22 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function AutoBind(_, _1, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjustedDescriptor;
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
@@ -15,13 +33,40 @@ class ProjectInput {
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
+    gatherUserInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+        if (enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredPeople.trim().length === 0) {
+            alert('Invalid injput, prlease try again later');
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+    clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
     submitHandler(event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+        }
+        this.clearInputs();
     }
     configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
 }
+__decorate([
+    AutoBind
+], ProjectInput.prototype, "submitHandler", null);
 const projectInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
+// Decorator: AutoBind
+// Automatically binds a class method to its instance, ensuring proper context.
+function AutoBind (_: any, _1: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value; //accessing body of the original method.
+
+    // Adjusted descriptor with binding behavior.
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get(){
+            const boundFn = originalMethod.bind(this); // Bind the method to the class instance.
+            return boundFn;
+        }
+    };
+    return adjustedDescriptor;
+}
+
+// Class: ProjectInput
+// Represents an input form with various methods to handle user interaction.
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -8,43 +28,90 @@ class ProjectInput {
     peopleInputElement: HTMLInputElement;
 
     constructor() {
-        // Get the <template> element with the id 'project-input'
+        //TODO:1 Get the <template> element with the id 'project-input'
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
 
-        // Get the <div> element with the id 'app' where the form will be rendered
+        //TODO: 2 Get the <div> element with the id 'app' where the form will be rendered
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
-        // Import the content of the <template> element (including its contents)
+        //TODO: 3 Import the content of the <template> element (including its contents)
         const importedNode = document.importNode(this.templateElement.content, true);
 
-        // Get the first child element of the imported node, which is the <form> element
+        //TODO: 4 Get the first child element of the imported node, which is the <form> element
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id ='user-input';
 
+        // Assign input elements
         this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
         this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
         this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
 
-        // Call the private method to attach the form to the host element
+        //TODO: 5 Call the private method to attach the form to the host element
         this.attach();
         this.configure();
     }
 
-    // Method to insert the form element into the host element
+    // Method: attach
+    // Inserts the form element into the host element.
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 
-    private submitHandler(event: Event){
-        event.preventDefault();
-        console.log(this.titleInputElement.value);
+    // TODO: 6. Method to validate user input. Using tuple with three values.
+    private gatherUserInput():[string, string, number]| void{
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        // ... (user input validation logic)
+        if(
+            enteredTitle.trim().length=== 0||
+            enteredDescription.trim().length===0||
+            enteredPeople.trim().length===0
+        ){
+            alert('Invalid injput, prlease try again later');
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
     }
 
+    // Method: clearInputs
+    // Clears input fields.
+      private clearInputs() {
+        this.titleInputElement.value='';
+        this.descriptionInputElement.value='';
+        this.peopleInputElement.value='';
+    }
+
+    // When the autobind decorator is used, it automatically binds class methods
+    // to the instance of the class where they are defined. T
+    @AutoBind
+    private submitHandler(event: Event){
+        event.preventDefault();
+        // TODO: 7. Collect user input values using an external method.
+        const userInput = this.gatherUserInput();
+
+        // TODO: 8. Destructure the array of three values.
+        if (Array.isArray(userInput)){
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+        }
+
+        //TODO: 9. clear all the input in the form by external method
+        this.clearInputs();
+
+    }
+
+    // Method: configure
+    // Configures event listeners and bindings.
     private configure(){
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        // TODO: 10. Configure the event listener for form submission
+        this.element.addEventListener('submit', this.submitHandler);
     }
 }
 
-
+// Create an instance of the ProjectInput class
 const projectInput= new ProjectInput();
 
